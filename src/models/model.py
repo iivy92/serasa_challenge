aca9a3a30e14c2ae8f9e92ab6e0acac8b96dbe51
Pedro Ivo <pedro.santana@hbsis.com.br>
2020-02-22 00:15:10 -0500

    formating output from the credit transaction

--- src/models/model.py
+++ src/models/model.py
@@ -7,12 +7,12 @@ class CreditTransaction():
     def __init__(self):
         self.constant = CalculationValues()
         self.violation = Violations()
-
     
-    def trasaction(self, data):
+    def transport_trasaction(self, data):
         data = json.loads(data)
+        violations = [self._transport_transaction(data), self._income_validation(data), self._score_validation(data)]
+        return {"transaction": {"id":data["transaction"]["id"], "violations":violations}}
 
-        
     def _income_validation(self,data):
         quota = int(data["transaction"]["requested_value"]) / int(data["transaction"]["installments"])
         committed_income = int(data["transaction"]["income"]) * self.constant.compromised_rate
@@ -26,5 +26,4 @@ class CreditTransaction():
     def _installments_validation(self,data):
         if (int(data["transaction"]["installments"]) < self.constant.minimum_installments)):
             return self.violation.minimum_installments
-
 
