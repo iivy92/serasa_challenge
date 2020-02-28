c906ff891b058d7b022cc70f8d864e594242b4f4
Pedro Ivo <pedro.santana@hbsis.com.br>
2020-02-27 21:52:38 -0500

    putting some logs messages

--- src/views.py
+++ src/views.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from flask import request
 from src.models.model import credit_transactions
+from src.utils.logger import logger
 import json
 import http
 
@@ -10,8 +11,11 @@ def __init__(self):
 def credit_transaction():
     data = request.get_json()
     try:
+        logger.info(f'Trying to authorize transaction for consumer: {data["transaction"]["id"]}')
         output = credit_transactions.transport_trasaction(data)
         credit_transactions.save_transaction(data)
+        logger.info(f'Transaction processed for consumer: {data["transaction"]["id"]}')
         return jsonify({"transaction": output}), http.HTTPStatus.ACCEPTED
     except Exception as e:
-        return jsonify({'msg': "Error", 'AdditionalData': str(e)}), http.HTTPStatus.BAD_REQUEST
+        logger.info(f'Something bad happened with the transaction. Error: {repr(e)}')
+        return jsonify({'msg': "Error", 'AdditionalData': repr(e)}), http.HTTPStatus.BAD_REQUEST
